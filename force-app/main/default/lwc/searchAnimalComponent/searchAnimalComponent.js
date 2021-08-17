ef022f4b974926df98243b0108124796f8438d3c
Tim <artemlysechko@gmail.com>
2021-08-17 19:51:45 +0300

    lwc search added

--- force-app/main/default/lwc/searchAnimalComponent/searchAnimalComponent.js
+++ force-app/main/default/lwc/searchAnimalComponent/searchAnimalComponent.js
@@ -1,56 +1,37 @@
-import { LightningElement,track} from 'lwc';
-// import server side apex class method 
+import { LightningElement, track } from 'lwc';
 import getAnimalList from '@salesforce/apex/SearchAnimalLWC.getAnimalList';
-// import standard toast event 
-import {ShowToastEvent} from 'lightning/platformShowToastEvent'
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
  
-export default class customSearch extends LightningElement {
-    
-    @track animalRecords;
+export default class SearchAnimalComponent extends LightningElement {
+    @track animals;
+    @track error;
     searchValue = '';
-    error;
-    results;
- 
-    // update searchValue var when input field value change
+
     searchKeyword(event) {
         this.searchValue = event.target.value;
         console.log('onchange searchValue: ' + this.searchValue)
     }
- 
-    // call apex method on button click 
+
     handleSearchKeyword() {
-        
-        if (this.searchValue != '') {
-            console.log('onclick searchValue: ' + this.searchValue)
-            getAnimalList({
-                    'searchKey' : this.searchValue
-                })
-                console.log('getAnimalList called')
-                .then((result) => {
-                    console.log("result: " + result);
-                    // set @track contacts variable with return contact list from server  
-                    this.animalRecords = result;
-                    console.log('result assigned: ' + this.animalRecords);
-                })
-                .catch((error) => {
-                    this.error = error.body.message;
-                    console.log('error catched: ' + this.error);
-                    //const event = new ShowToastEvent({
-                    //    variant: 'error',
-                    //    message: error.body.message,
-                    });
-                    //this.dispatchEvent(event);
-                    // reset contacts var with null
-                    //console.log('reset contacts var with null')   
-                    //this.animalsRecord = null;
-                //});
-        } else {
-            // fire toast event if input field is blank
-            const event = new ShowToastEvent({
-                variant: 'error',
-                message: 'Search text missing..',
+        console.log('onclick searchValue: ' + this.searchValue);
+        getAnimalList({searchKey : this.searchValue})
+            .then(result => {
+                console.log('result: ' + JSON.stringify(result));
+                this.animals = result;
+            })
+            .catch(error => {
+                console.log('error: ' + error.body.message);
+                this.error = error.body.message;
+
+                const event = new ShowToastEvent({
+                        variant: 'error',
+                        message: error.body.message,
+                });
+                this.dispatchEvent(event);
+                // reset contacts var with null
+                console.log('reset contacts var with null')   
+                this.animalsRecord = null;
+
             });
-            this.dispatchEvent(event);
-        }
     }
 }
